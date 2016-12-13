9a2f87235d0f52fb7407fd4eed68aec8770b2f1c
dadiletta <dan.adiletta@gmail.com>
2016-12-12 20:59:37 -0500

    added relay, additional logic

--- xmas.py
+++ xmas.py
@@ -1,5 +1,6 @@
 ######################
 ##### LED ALARM CLOCK - Using Adafruit's LED strips and Dexter Industry's GrovePi
+##### TODO: Solve this weird global issue. Switch to instance variables?
 ######################
 
 from neopixel import *
@@ -8,7 +9,12 @@ from grove_rgb_lcd import *
 import grovepi
 import time
 import datetime
+import RPi.GPIO as GPIO
 
+# relay attempt 1
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.OUT)
+GPIO.output(26, GPIO.LOW)
 ######################
 ##### LED INFO
 ######################
@@ -21,26 +27,25 @@ LED_DMA = 3  # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 254  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
 strip1 = Adafruit_NeoPixel(LED_COUNT, LED_PIN1, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
-#strip2 = Adafruit_NeoPixel(LED_COUNT, LED_PIN2, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+strip2 = Adafruit_NeoPixel(LED_COUNT, LED_PIN2, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
 
 ######################
 ##### ALARM TIMES
 ######################
 
+TIME_TO_PREHEAT = 15
 TIME_TILL_ANNOY = 10
 TIME_TILL_SHUTOFF = 20
 # alarm toggle
-global alarm_set
 alarm_set = False
 # load a default value for time
-global alarm_time
 alarm_time = datetime.datetime.now()
 # this alarm only needs hours and minutes
 alarm_time = alarm_time.replace(year=2017, month=1, day=1, second=0, microsecond=0)
-global screen_on
 screen_on = False
-global alarm_running
 alarm_running = False
+# shhh is used to prevent the alarm from coming right back on when silenced
+shhh = False
 
 ######################
 ##### GROVEPI SETUP
@@ -74,32 +79,36 @@ def colorWipe(strip, color, wait_ms=50):
 
 def wakeup():
     print('Waking up')
-    global strip1
-    global alarm_running
-    #global strip2
-    # Intialize the library (must be called once before other functions).
+    global strip1, alarm_running, strip2
+    # Intialize the neopixel library (must be called once before other functions).
     strip1.begin()
+    strip2.begin()
     colorWipe(strip1, Color(255, 255, 255))
     alarm_running = True
+    # TODO: trigger relay for heater
 
 
 def shutoff():
-    global strip1
-    #global strip2
-    global alarm_running
+    global strip1, strip2, alarm_running, shhh
     alarm_running = False
+    # prevent alarm from coming right back on
+    shhh = True
     print('Shutting off')
     killScreen()
+    # turn off the heater
+    GPIO.output(26, GPIO.LOW)
+    # turn off the lights
     if strip1:
         colorWipe(strip1, Color(0, 0, 0), wait_ms=10)
-        #strip1.__del__()
-    #if strip2:
-        #colorWipe(strip2, Color(0, 0, 0), wait_ms=10)
-        #strip2.__del__()
+    if strip2:
+        colorWipe(strip2, Color(0, 0, 0), wait_ms=10)
 
 def annoy():
     print('Snooze is over')
+    # TODO: Buzzer
+    # TODO: Rainbow
 
+# send message to GrovePi LCD screen
 def showStatus(highlight):
     global screen_on
     print('Displaying screen with %s highlighted') % highlight
@@ -124,10 +133,11 @@ def killScreen():
         time.sleep(.05)
         screen_on = False
 
+# display options
 def showMenu():
-    global alarm_set
-    global screen_on
-    global alarm_time
+    # load our variables; because defining scope is weird
+    global alarm_set, screen_on, alarm_time
+    # timer to auto shut off the screen after 5 min
     menu_start = datetime.datetime.now()
     while datetime.datetime.now() < menu_start + datetime.timedelta(minutes=5):
         # this check is to avoid flashing the LCD screen repeatidly
@@ -143,11 +153,14 @@ def showMenu():
                 if not screen_on:
                     showStatus("toggle")
                 time.sleep(.5)
+                # button1 will exit
                 if grovepi.digitalRead(button1) == 1:
                     # let's double-check      
                     time.sleep(.1)
+                    # exit the menu
                     if grovepi.digitalRead(button1) == 1:
                         return
+                # button2 toggles the alarm
                 if grovepi.digitalRead(button2) == 1:
                     alarm_set = not alarm_set
                     screen_on = False
@@ -167,45 +180,37 @@ if __name__ == '__main__':
 
     # MAIN APP LOOP
     while True:
+
         current_time = datetime.datetime.now()
         current_time = current_time.replace(year=2017, month=1, day=1, second=0, microsecond=0)
-        if alarm_set and current_time == alarm_time and not alarm_running:
+        # TODO: Prevent alarm from restarting after being disabled
+
+        # trigger floor heater in advance of alarm
+        if alarm_set and current_time == (alarm_time - datetime.timedelta(minutes = TIME_TO_PREHEAT)):
+            # try to trigger the heater
+            GPIO.output(26, GPIO.HIGH)
+
+        # trigger alarm
+        if alarm_set and current_time == alarm_time and not alarm_running and not shhh:
             wakeup()
-        if alarm_set and current_time == (alarm_time + datetime.timedelta(minutes = TIME_TILL_ANNOY)):
+        # annoy you until awake
+        if alarm_set and alarm_running and current_time == (alarm_time + datetime.timedelta(minutes = TIME_TILL_ANNOY)):
             annoy()
-        if alarm_set and current_time == (alarm_time + datetime.timedelta(minutes = TIME_TILL_SHUTOFF)):
+        # auto shut-off
+        if alarm_set and alarm_running and current_time == (alarm_time + datetime.timedelta(minutes = TIME_TILL_SHUTOFF)):
             shutoff()
+        # button2 disables the alarm
         if grovepi.digitalRead(button2) == 1:
             shutoff()
-        ## show screen
+        ## show screen options
         if grovepi.digitalRead(button1) == 1:
             showMenu()
+
+        if current_time > alarm_time:
+            shhh = False
 
         killScreen()
         message = ("%s : %s || On: " + str(alarm_set)) % (alarm_time.strftime("%H"), alarm_time.strftime("%M"))
         print("button1: %d || button2: %d \nAlarm: %s\n") % (grovepi.digitalRead(button1), grovepi.digitalRead(button2), message)
+        # the longer this is, the longer you have to hold the button
         time.sleep(.5)
-        
-
-
-'''
-    # Create NeoPixel object with appropriate configuration.
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
-    # Intialize the library (must be called once before other functions).
-    strip.begin()
-
-    print('Press Ctrl-C to quit.')
-    while True:
-        # Color wipe animations.
-        colorWipe(strip, Color(255, 0, 0))  # Red wipe
-        colorWipe(strip, Color(0, 255, 0))  # Blue wipe
-        colorWipe(strip, Color(0, 0, 255))  # Green wipe
-        # Theater chase animations.
-        theaterChase(strip, Color(127, 127, 127))  # White theater chase
-        theaterChase(strip, Color(127, 0, 0))  # Red theater chase
-        theaterChase(strip, Color(0, 0, 127))  # Blue theater chase
-        # Rainbow animations.
-        rainbow(strip)
-        rainbowCycle(strip)
-        theaterChaseRainbow(strip)
-'''
